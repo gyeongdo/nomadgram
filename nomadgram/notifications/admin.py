ac8f4fec09b0337f856362c8c55b5c3f8aaea20d
veneziar1 <venezia5933@naver.com>
2018-11-17 23:39:23 +0900

    1-72-reload

---
+++ nomadgram/notifications/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Notification)
+class NotificationAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'creater',
+        'to',
+        'notification_type'
+    )
